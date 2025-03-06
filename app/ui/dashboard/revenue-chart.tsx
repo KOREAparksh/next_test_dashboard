6b2c30b5b045096bd57dc1a56a28d619d56459f2
Park Seunghan <tmdgks2222@naver.com>
2025-03-06 16:37:55 +0900

    챕터9까지

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,15 +1,17 @@
-import { Revenue } from "@/app/lib/definitions";
 import { generateYAxis } from "@/app/lib/utils";
 import { lusitana } from "@/app/ui/fonts";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 
+import { fetchRevenue } from "@/app/lib/data";
 // This component is representational only.
 // For data visualization UI, check out:
 // https://www.tremor.so/
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) {
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
